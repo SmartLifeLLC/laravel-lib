d5374c6921b3f4166cdd5b89c755668fcc450d43
Bhags <jungsinyu@gmail.com>
2018-01-29 00:04:33 +0900

    version upgraded

--- prototype/app/Constants/Versions.php
+++ prototype/app/Constants/Versions.php
@@ -13,7 +13,7 @@ namespace App\Constants;
 
 final class Versions
 {
-        const CURRENT = "1";
-        const ALLOWED = ["v2017-07-31"];
+        const CURRENT = "2";
+        const ALLOWED = ["1","v2017-07-31"];
         const AUTH_ALLOW_VERSION = "v2017-07-31";
 }
